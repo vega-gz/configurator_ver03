53fd71d91de39c8018071fa90cd6281caf60efe6
ad <ad@dnaza-arch>
2020-04-08 20:52:39 +0300

    Собственный класс таблицы, так же прикрутил туда и базы и методы поиска по имени столбца

--- src/FrameCreate/TableNzVer2.java
+++ src/FrameCreate/TableNzVer2.java
@@ -138,77 +138,6 @@ public class TableNzVer2 extends javax.swing.JPanel {
             }
         }
         return new NZDefaultTableModel(dataInTable, resultColumn, nameTable); 
-//        return new DefaultTableModel(dataInTable, resultColumn) { // название столбцов resultColumn
-//            @Override
-//            public Class<?> getColumnClass(int columnIndex) { // структура для отображения таблицы с галками
-//                Class clazz = String.class;
-//                switch (columnIndex) {
-//                    case 0:
-//                        clazz = Boolean.class;
-//                        break;
-//                }
-//                return clazz;
-//            }
-//
-//            @Override
-//            public boolean isCellEditable(int row, int column) {
-//                return column == column;
-//            }
-//
-//            @Override
-//            public void setValueAt(Object aValue, int row, int column) {
-//                // Условие проверки галочки скрывать легенду
-//                if (aValue instanceof Boolean && column == 0) {
-//                    //System.out.println("Posution - > " + row + " " + aValue);        
-//                    Vector rowData = (Vector) getDataVector().get(row); // не помню для чего но без этого только скрывает =(
-//                    rowData.set(0, (boolean) aValue);
-//                    fireTableCellUpdated(row, column);
-//
-//                    try {
-//                        // Само действие не реализованно
-//                        if ((boolean) aValue == true) {
-//                            System.out.println("true");
-//                        }
-//                        if ((boolean) aValue == false) {
-//                            System.out.println("false");
-//                        }
-//                    } catch (NullPointerException e) {
-//                        JOptionPane.showMessageDialog(null, "Трудности с добавлением");
-//                    }
-//                } else {  // если нет Галки просто обновляем данные
-//                    if (workbase != null) { // если вызов без редактирования базы
-//                        Vector rowData = (Vector) getDataVector().get(row); // не помню для чего но без этого только скрывает =(
-//                        //String curentCell = (String) rowData.get(column); // получить предыдущие данные
-//                        //String ColumnName = jTable1.getColumnName(column); // Имя выделенного столбца
-//                        String ColumnName = resultColumn[column];
-//                        //int rowTM = jTable1.getRowCount(); // количество строк
-//                        //int ColumnTM =  jTable1.getColumnCount();  //Количество столбцов
-//                        int ColumnTM = resultColumn.length;
-//                        HashMap< String, String> mapDataRow = new HashMap<>(); // элементы для отображения в этих полях
-//                        for (int i = 1; i < ColumnTM; ++i) { // Пробежать по строке где изменяются данные и сформировать список для обновления данных в базе c 1 так как там галки
-//                            //mapDataRow.put(jTable1.getColumnName(i), (String) rowData.get(i)); // Формируем список данных принудительно в String
-//                            mapDataRow.put(resultColumn[i], (String) rowData.get(i)); // Формируем список данных принудительно в String
-//                        }
-//                        workbase.Update(nameTable, ColumnName, (String) aValue, mapDataRow); // обновить данные ячейки в таблицы базы
-//                        rowData.set(column, aValue); // Вставляем новые данные в нужную ячейку( только после этого вставляем ячейку иначе в базу неправильный запрос пойдет)
-//                        //System.out.println("columnTM " + columnTM + " yT " + yT  );
-//                    }
-//                }
-//
-//            }
-//             // --- Собственный метод получить данные из таблицы по имени столбца  ---
-//            public Object getDataNameColumn(String nameColumn, int row){
-//                Object objTable = null;
-//                for(int i=0; i<resultColumn.length; ++i){ // Пробегаем по всем нашим именам столбцов как они стоят 
-//                    if(nameColumn.equals(resultColumn[i])){ //  нашли совпадение
-//                        objTable = getValueAt(i, row);
-//                        System.out.println(getValueAt(i, row));
-//                    }
-//                }
-//                return objTable;
-//            }
-//        };
-        
     }
 
     /**
@@ -260,7 +189,6 @@ public class TableNzVer2 extends javax.swing.JPanel {
             JTableHeader th = jTable1.getTableHeader();
             th.repaint(); // без этого не работает отрисовка , тут с ней тоже
         }
-
     }//GEN-LAST:event_jTable1MousePressed
 
 // -- мини меню по мыши первого столбца таблицы---
@@ -306,9 +234,6 @@ public class TableNzVer2 extends javax.swing.JPanel {
         this.nameTable = nameTable;
        }
       
-      public NZDefaultTableModel (){
-        //super();
-      }
             @Override
             public Class<?> getColumnClass(int columnIndex) { // структура для отображения таблицы с галками
                 Class clazz = String.class;
