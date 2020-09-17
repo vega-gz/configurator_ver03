d7f8468d5fee1683f2d047502494686d732944c7
gCherepanov <gregtox@mail.ru>
2020-09-17 08:44:20 +0300

    Поправил undo/redo

--- src/ReadWriteExcel/ExcelAdapter.java
+++ src/ReadWriteExcel/ExcelAdapter.java
@@ -3,14 +3,15 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ReadWriteExcel;
+ package ReadWriteExcel;
 
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.datatransfer.*;
 import java.util.*;
-import Tools.TableTools;
 
 /**
  * ExcelAdapter enables Copy-Paste Clipboard functionality on JTables. The
@@ -24,14 +25,27 @@ public class ExcelAdapter implements ActionListener {
     private Clipboard system;
     private StringSelection stsel;
     private JTable jTable1;
-    TableTools tt = new TableTools();
+
+    private TableCellListener listener;
+
+    private final UndoRedoSystem undoSystem = new UndoRedoSystem(15);
 
     /**
      * The Excel Adapter is constructed with a JTable on which it enables
      * Copy-Paste and acts as a Clipboard listener.
      */
     public ExcelAdapter(JTable myJTable) {
+//undo/redo
         jTable1 = myJTable;
+        listener = new TableCellListener(jTable1, new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TableCellListener tcl = (TableCellListener) e.getSource();
+                undoSystem.put(tcl.getOldData());
+            }
+        });
+        undoSystem.put( ((DefaultTableModel) jTable1.getModel()).getDataVector() );
+
         KeyStroke copy = KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK, false);
         // Identifying the copy KeyStroke user can modify this
         KeyStroke paste = KeyStroke.getKeyStroke(KeyEvent.VK_V, ActionEvent.CTRL_MASK, false);
@@ -39,11 +53,13 @@ public class ExcelAdapter implements ActionListener {
         KeyStroke delete = KeyStroke.getKeyStroke(KeyEvent.VK_DELETE, 0, false);
         // Identifying the Paste KeyStroke user can modify this
         KeyStroke cancel = KeyStroke.getKeyStroke(KeyEvent.VK_Z, ActionEvent.CTRL_MASK, false);
+        KeyStroke redo = KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK, false);
         // Identifying the Paste KeyStroke user can modify this
         jTable1.registerKeyboardAction(this, "Copy", copy, JComponent.WHEN_FOCUSED);
         jTable1.registerKeyboardAction(this, "Paste", paste, JComponent.WHEN_FOCUSED);
         jTable1.registerKeyboardAction(this, "Delete", delete, JComponent.WHEN_FOCUSED);
         jTable1.registerKeyboardAction(this, "Cancel", cancel, JComponent.WHEN_FOCUSED);
+        jTable1.registerKeyboardAction(this, "Redo", redo, JComponent.WHEN_FOCUSED);
         system = Toolkit.getDefaultToolkit().getSystemClipboard();
     }
 
@@ -57,7 +73,20 @@ public class ExcelAdapter implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().compareTo("Cancel") == 0) {
-          
+            TableModel model = jTable1.getModel();
+            Vector<Vector> data = undoSystem.undo();
+            if (data == null) return;
+            for (int i = 0; i < data.size(); ++i)
+                for (int j = 0; j < data.get(0).size(); ++j)
+                    model.setValueAt(data.get(i).get(j), i, j);
+        }
+        if (e.getActionCommand().compareTo("Redo") == 0) {
+            TableModel model = jTable1.getModel();
+            Vector<Vector> data = undoSystem.redo();
+            if (data == null) return;
+            for (int i = 0; i < data.size(); ++i)
+                for (int j = 0; j < data.get(0).size(); ++j)
+                    model.setValueAt(data.get(i).get(j), i, j);
         }
         if (e.getActionCommand().compareTo("Delete") == 0) {//удаление ячейки по кнопке
             int numcols = jTable1.getSelectedColumnCount();
@@ -73,11 +102,14 @@ public class ExcelAdapter implements ActionListener {
                         JOptionPane.ERROR_MESSAGE);
                 return;
             }
+
+            listener.fixOldData();
             for (int i = 0; i < numrows; i++) {
                 for (int j = 0; j < numcols; j++) {
                     jTable1.setValueAt("", rowsselected[i], colsselected[j]);
                 }
             }
+            listener.tableChanged();
         }
         if (e.getActionCommand().compareTo("Copy") == 0) {
             StringBuffer sbf = new StringBuffer();
@@ -115,6 +147,7 @@ public class ExcelAdapter implements ActionListener {
                 String trstring = (String) (system.getContents(this).getTransferData(DataFlavor.stringFlavor));
                 System.out.println("String is:" + trstring);
                 StringTokenizer st1 = new StringTokenizer(trstring, "\n");
+                listener.fixOldData();
                 for (int i = 0; st1.hasMoreTokens(); i++) {
                     rowstring = st1.nextToken();
                     StringTokenizer st2 = new StringTokenizer(rowstring, "\t");
@@ -127,20 +160,39 @@ public class ExcelAdapter implements ActionListener {
                         System.out.println("Putting " + value + "atrow=" + startRow + i + "column=" + startCol + j);
                     }
                 }
+                listener.tableChanged();
             } catch (Exception ex) {
                 ex.printStackTrace();
             }
         }
-        if (e.getActionCommand().compareTo("Paste") == 0) {
-            System.out.println("Trying to Paste");
-            int statrRow = (jTable1.getSelectedRows())[0];
-            int startCol = (jTable1.getSelectedColumns())[0];
-            try {
-                String trstring = (String) (system.getContents(this).getTransferData(DataFlavor.stringFlavor));
-                System.out.println("String is:"+trstring);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
+    }
+    private static class UndoRedoSystem {
+        private final Deque<Vector<Vector>> history = new LinkedList<>();
+        private final Deque<Vector<Vector>> trash = new LinkedList<>();
+        private final int capacity;
+
+        public UndoRedoSystem(int capacity) {
+            this.capacity = capacity;
+        }
+
+        public Vector<Vector> undo() {
+            if (history.isEmpty()) return null;
+            Vector<Vector> data = history.removeLast();
+            trash.addLast(data);
+            return data;
+        }
+        public Vector<Vector> redo() {
+            if (trash.isEmpty()) return null;
+            Vector<Vector> data = trash.removeLast();
+            history.addLast(data);
+            return data;
+        }
+
+        public void put(Vector<Vector> data) {
+            trash.clear();
+            history.addLast(data);
+            if (history.size() == capacity)
+                history.removeFirst();
         }
     }
 }
